e1eab0ecec5ad9d70003c809cc1e8f48de3d5405
Vlad Probst <vladlen.probst@gmail.com>
2025-02-11 23:18:43 +0200

    filters start

---
+++ client/inventory-managment-system/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FiltersComponent } from './components/filters/filters/filters.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    FiltersComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FiltersComponent
+  ]
+})
+export class SharedModule { }
